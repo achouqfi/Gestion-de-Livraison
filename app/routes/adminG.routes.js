b2bfe75d278c244c562f50c82fac7be08e28dcf7
achouqfi <75440007+achouqfi@users.noreply.github.com>
2022-01-21 16:48:28 +0100

    last commit

--- app/routes/adminG.routes.js
+++ app/routes/adminG.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan')
+const fs = require('fs')
 const router = express.Router();
 const {
     adminGGet,
@@ -7,7 +8,7 @@ const {
 } = require('../controllers/adminG.controller')
 
 //method routes adminG
-router.get('/',morgan('combined'),adminGGet)
+router.get('/',morgan('common', {stream: fs.createWriteStream('./access.log', {flags: 'a'})}),adminGGet)
 router.post('/login',morgan('combined'),login)
 
 module.exports = router;
