2eda9aa504553507f34bb333166be0c25ae75262
achouqfi <75440007+achouqfi@users.noreply.github.com>
2022-01-24 13:44:26 +0100

    last commit

--- app/controllers/commande.controller.js
+++ app/controllers/commande.controller.js
@@ -96,16 +96,46 @@ exports.CommandeDelete = async(req,res)=>{
 
 //update commande
 exports.CommandeUpdate = async(req,res)=>{
-    const data= req.params.id;
-    try {
-        const updatedcommande = await commande.updateOne(
-            {_id:data},
-            {$set: {name:req.body.name, email:req.body.email}}
-        )
-        res.json(updatedcommande)
-    } catch (err) {
-        res.status(400).json({ message: err.message })
+    const data = req.body;
+    let prix;
+    if(data.poids > 3){
+        prix = (data.poids - 3) * 5 + 120;
+    }else{
+        prix = data.poids * 40;
     }
+    const response = await axios(`https://www.distance24.org/route.json?stops=${data.ville_depart}%7C${data.ville_arrive}`)
+
+    try{
+        const updatedcommande = await commande.updateOne(
+            {_id:req.params.id},
+            {$set: {
+                heure:data.heure,
+                depart:data.depart,
+                ville_depart:data.ville_depart,
+                ville_arrive:data.ville_arrive,
+                poids:data.poids,
+                prix: prix,
+                distance_kilometrage:response.data.distances.join(),
+            }}
+        )
+        const chauffeurGet = await chauffeur.find().populate("camion");
+        chauffeurGet.forEach(element => {
+            if(element.camion.type == "voiture" && data.poids > 0  && data.poids <= 200 ){
+                console.log(element.email);
+                email(element.email)
+            }else if(element.camion.type == "petit" && data.poids >= 200 && data.poids <= 800 ){
+                console.log(element.email);
+                email(element.email)
+            }else if(element.camion.type == "grand" && data.poids >= 800 && data.poids <= 1600 ){
+                console.log(element.email);
+                email(element.email)
+            }
+        });
+        res.status(201).json(newcommande);
+    }catch(err){
+        res.status(404).json({ message : err.message })
+    }
+
 }
 
 //update commande status
